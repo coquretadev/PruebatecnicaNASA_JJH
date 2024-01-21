61ebd5cb9769431206933ae36d6663ae205a0475
Julls Jimenez Hernandez <jjimenezh.ext@quironsalud.es>
2024-01-21 20:44:08 +0100

    Desarrollo de la lógica de negocio del endpoint

--- PruebatecnicaNASA_JJH/App/Services/AsteroidService.cs
+++ PruebatecnicaNASA_JJH/App/Services/AsteroidService.cs
@@ -1,12 +1,64 @@
-﻿using PruebatecnicaNASA_JJH.App.Dto;
+﻿using Newtonsoft.Json;
+using PruebatecnicaNASA_JJH.App.Dto;
 
 namespace PruebatecnicaNASA_JJH.App.Services
 {
     public class AsteroidService : IAsteroidService
     {
+        private readonly IConfiguration _configuration;
+
+        public AsteroidService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
         public async Task<IEnumerable<AsteroidResponseDto>> GetAsteroidsAsync(int days)
         {
-            throw new NotImplementedException();
+            var result = new List<AsteroidResponseDto>();
+
+            // Calcular fechas
+            DateTime startDate = DateTime.Now.Date;
+            DateTime endDate = startDate.AddDays(days);
+
+            // Obtener datos de la API de la NASA
+            var url = _configuration["AppSettingsNasa:api_url"];
+            var key = _configuration["AppSettingsNasa:api_key"];
+
+            string apiUrl = $"{url}?start_date={startDate:yyyy-MM-dd}&end_date={endDate:yyyy-MM-dd}&api_key={key}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception($"Error en la solicitud a la API de la NASA: {response.ReasonPhrase}. StatusCode {(int)response.StatusCode}");
+                }
+
+                string jsonContent = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<NasaApiResponseDto>(jsonContent);
+
+                // Filtrar asteroides potencialmente peligrosos y ordenar por tamaño y Construir respuesta en el formato requerido
+                result = data.NearEarthObjects
+                    .SelectMany(pair => pair.Value)
+                    .Where(asteroid => asteroid.IsPotentiallyHazardousAsteroid)
+                    .OrderByDescending(asteroid => asteroid.EstimatedDiameter.Kilometers.EstimatedDiameterMax)
+                    .Take(3)
+                    .Select(asteroid => new AsteroidResponseDto
+                    {
+                        //id = asteroid.Id,
+                        Name = asteroid.Name,
+                        Diameter = (asteroid.EstimatedDiameter.Kilometers.EstimatedDiameterMin + asteroid.EstimatedDiameter.Kilometers.EstimatedDiameterMax) / 2,
+                        Velocity = asteroid.CloseApproachData.First().RelativeVelocity.KilometersPerHour,
+                        Date = asteroid.CloseApproachData.First().CloseApproachDate,
+                        Planet = asteroid.CloseApproachData.First().OrbitingBody
+                        //isPotentiallyHazardousAsteroid = asteroid.IsPotentiallyHazardousAsteroid,
+                    })
+                    .ToList();
+
+                return result;
+            }
         }
     }
 }
--- PruebatecnicaNASA_JJH/Program.cs
+++ PruebatecnicaNASA_JJH/Program.cs
@@ -1,3 +1,5 @@
+using PruebatecnicaNASA_JJH.App.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IAsteroidService, AsteroidService>(); // Inyección de dependecias del Service
 
 var app = builder.Build();
 
