21dd81e47cb92357c8b099eacaee5d09c80d1b4f
Julls Jimenez Hernandez <jjimenezh.ext@quironsalud.es>
2024-01-21 20:22:21 +0100

    Se prepara la capa de Aplicacion del entorno

---
+++ PruebatecnicaNASA_JJH/Controllers/AsteroidController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PruebatecnicaNASA_JJH.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AsteroidController : ControllerBase
+    {
+        
+    }
+}
