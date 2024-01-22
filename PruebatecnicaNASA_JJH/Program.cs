bcf38e0c47a507bef4d3065ebb5a4dee9cf86b55
Julls Jimenez Hernandez <jjimenezh.ext@quironsalud.es>
2024-01-22 02:29:41 +0100

    Se crea archivo CheatSheet.txt para hacer la migración desde la consola

--- PruebatecnicaNASA_JJH/Program.cs
+++ PruebatecnicaNASA_JJH/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using PruebatecnicaNASA_JJH.App.Services;
+using PruebatecnicaNASA_JJH.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +11,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IAsteroidService, AsteroidService>(); // Inyección de dependecias del Service
+//builder.Configuration.GetConnectionString("AsteroidEntityConnection");
+builder.Services.AddDbContext<AsteroidContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AsteroidEntityConnection"))
+);
 
 var app = builder.Build();
+
+//// Cuando se ejecuta el proyecto, crea la bd:
+//using (var scope = app.Services.CreateScope())
+//{
+//    var dataEntityContext = scope.ServiceProvider.GetRequiredService<AsteroidContext>();
+//    dataEntityContext.Database.Migrate();
+//}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
