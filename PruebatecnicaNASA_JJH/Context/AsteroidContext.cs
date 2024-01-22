9016bd31c94eee323456e004486c964ee60cf137
Julls Jimenez Hernandez <jjimenezh.ext@quironsalud.es>
2024-01-22 01:31:47 +0100

    Se prepara el entorno para la migración

---
+++ PruebatecnicaNASA_JJH/Context/AsteroidContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebatecnicaNASA_JJH.Entities;
+
+namespace PruebatecnicaNASA_JJH.Context
+{
+    public class AsteroidContext : DbContext
+    {
+        public AsteroidContext(DbContextOptions<AsteroidContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<AsteroidEntity> AsteroidEntity { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
---
+++ PruebatecnicaNASA_JJH/Entities/AsteroidEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace PruebatecnicaNASA_JJH.Entities
+{
+    public class AsteroidEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsPotentiallyHazardous { get; set; }
+
+        public double EstimatedDiameterMin { get; set; }
+
+        public double EstimatedDiameterMax { get; set; }
+
+        public double RelativeVelocityKilometersPerHour { get; set; }
+
+        public DateTime CloseApproachDate { get; set; }
+
+        public string OrbitingBody { get; set; }
+    }
+}
