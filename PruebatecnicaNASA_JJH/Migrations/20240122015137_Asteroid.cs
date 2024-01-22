5208245d9aaee4dc94ea88e909080889e716c640
Julls Jimenez Hernandez <jjimenezh.ext@quironsalud.es>
2024-01-22 02:52:51 +0100

    Se crea la migración

---
+++ PruebatecnicaNASA_JJH/Migrations/20240122015137_Asteroid.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PruebatecnicaNASA_JJH.Migrations
+{
+    /// <inheritdoc />
+    public partial class Asteroid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Asteroid",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Diameter = table.Column<double>(type: "float", nullable: false),
+                    Velocity = table.Column<double>(type: "float", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Planet = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Asteroid", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Asteroid");
+        }
+    }
+}
